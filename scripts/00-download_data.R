569d64badcf7455b791502f4a888a446a72f99b6
mickeyymousee <sarah.mansoor@mail.utoronto.ca>
2022-06-14 16:11:20 -0400

    Download and rename files
    Change file names to a more readable name

--- scripts/00-download_data.R
+++ scripts/00-download_data.R
@@ -15,7 +15,7 @@ library(tidyverse)
 library(dplyr)
 
 #### Load datasets ####
-temp_jan_may_2022 <- read.csv("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2022_P1D")
+temp_jan_may_2022 <- read.csv("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2022_P1D.csv")
 temp_jan_dec_2021 <- read.csv("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2021_P1D.csv")
 temp_jan_dec_2020 <- read.csv("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2020_P1D.csv")
 temp_jan_dec_2019 <- read.csv("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2019_P1D.csv")
@@ -37,7 +37,28 @@ temp_jan_dec_2004 <- read.csv("~/Desktop/SUDS2022/inputs/data/en_climate_daily_O
 temp_jan_dec_2003 <- read.csv("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2003_P1D.csv")
 temp_jan_dec_2002 <- read.csv("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2002_P1D.csv")
 
-
+# Rename files to be more readable
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2022_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_may_2022.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2021_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2021.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2020_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2020.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2019_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2019.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2018_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2018.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2017_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2017.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2016_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2016.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2015_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2015.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2014_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2014.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2013_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2013.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2012_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_may_2012.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2011_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2011.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2010_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2010.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2009_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2009.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2008_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2008.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2007_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2007.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2006_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2006.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2005_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2005.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2004_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2004.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2003_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2003.csv")
+file.rename("~/Desktop/SUDS2022/inputs/data/en_climate_daily_ON_6158355_2002_P1D.csv", "~/Desktop/SUDS2022/inputs/data/temp_jan_dec_2002.csv")
 
 
 
