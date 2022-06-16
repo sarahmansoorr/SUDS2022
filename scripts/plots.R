c1cfbb300bd134c89a08b68faad8d9c990b434bd
mickeyymousee <sarah.mansoor@mail.utoronto.ca>
2022-06-16 15:55:39 -0400

    Update plots.R

--- scripts/plots.R
+++ scripts/plots.R
@@ -17,6 +17,8 @@ ggplot(temp_2002_2022, aes(x=month, y=min_temp, color=year)) + geom_point()
 
 ggplot(temp_2002_2022, aes(x=month, y=mean_temp, color=year)) + geom_point()
 
+ggplot(temp_2002_2022, aes(x=month_name, y=mean_temp, col=year)) + geom_col()
+
 
 #### tables ####
 library(knitr)
@@ -52,3 +54,40 @@ mean_temp_2002_2022 %>%
     col.names=c("Year", "Month", "Day", "Mean Temp"),
     linesep = "", digits = 2, booktabs=TRUE) %>%
   kable_styling(latex_options = "HOLD_position", font_size = 10)
+
+
+### summarising data
+summary_max_data <- temp_2002_2022 %>%
+  group_by(month_name) %>%
+  summarise(mean_max_temp = mean(max_temp))
+
+ggplot(summary_max_data) +
+  geom_col(aes(x = factor(month_name), y = mean_max_temp)) +
+  labs(title = "Mean Maximum Temperature by Month from 2002-2022", y = "Mean Maximum Temperature", 
+       x = "Month")
+
+summary_year_max_data <- temp_2002_2022 %>%
+  group_by(year) %>%
+  summarise(mean_max_temp = mean(max_temp))
+
+ggplot(summary_year_max_data) +
+  geom_col(aes(x = factor(year), y = mean_max_temp)) +
+  labs(title = "Mean Maximum Temperature by Year from 2002-2022", y = "Mean Maximum Temperature", 
+       x = "Year")
+
+
+### winter months
+
+winter <- temp_2002_2022 %>%
+  filter(month_name == "Dec" | month_name == "Jan" | month_name == "Feb" | month_name == "Mar")
+
+ggplot(winter, aes(x=year, y=max_temp, color=month_name)) + geom_point()
+
+spring <- temp_2002_2022 %>%
+  filter(month_name == "Apr" | month_name == "May" | month_name == "Jun" | month_name == "Jul")
+
+summer <- temp_2002_2022 %>%
+  filter(month_name == "Aug" | month_name == "Sep" | month_name == "Oct" | month_name == "Nov")
+
+
+
