6a16400007c82c4ee7ef00d137c431cbd38e14b1
mickeyymousee <sarah.mansoor@mail.utoronto.ca>
2022-06-29 12:05:11 -0400

    Update 02-data_analysis.R

--- scripts/02-data_analysis.R
+++ scripts/02-data_analysis.R
@@ -11,9 +11,41 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(knitr)
+library(kableExtra)
 library(ggplot2)
+library(dplyr)
+
+# Load temperature data
+temperature_2002_2022 <- read.csv("~/Desktop/SUDS2022/inputs/data/
+                                  clean_temp_data/temperature_2002_2022.csv")
+# Load pollution data
+poll_2011_2020 <- read.csv("~/Desktop/SUDS2022/inputs/data/
+                           clean_pollution_data/pollution_2011_2020.csv")
 
 ### Plots ----
+
+## Temperature ----
+
+# Visualizing Temperature by Months
+dec_mar <- temperature_2002_2022 %>%
+  filter(month_name == "Dec" | month_name == "Jan" | month_name == "Feb" | month_name == "Mar")
+
+ggplot(dec_mar, aes(x=year, y=max_temp, color=month_name)) + geom_point() + 
+  labs(color="Month") + xlab("Year") + ylab("Maximum Temperature")
+
+apr_jul <- temperature_2002_2022 %>%
+  filter(month_name == "Apr" | month_name == "May" | month_name == "Jun" | month_name == "Jul")
+
+ggplot(apr_jul, aes(x=year, y=max_temp, color=month_name)) + geom_point() + 
+  labs(color="Month") + xlab("Year") + ylab("Maximum Temperature")
+
+aug_nov <- temperature_2002_2022 %>%
+  filter(month_name == "Aug" | month_name == "Sep" | month_name == "Oct" | month_name == "Nov")
+
+ggplot(aug_nov, aes(x=year, y=max_temp, color=month_name)) + geom_point() + 
+  labs(color="Month") + xlab("Year") + ylab("Maximum Temperature")
+
 
 ### Models ----
 
