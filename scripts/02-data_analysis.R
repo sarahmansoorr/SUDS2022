0cfd68aea1e967149664016f8fa8c0a75b4a0b5c
mickeyymousee <sarah.mansoor@mail.utoronto.ca>
2022-07-19 10:15:57 -0400

    Updates to paper

--- scripts/02-data_analysis.R
+++ scripts/02-data_analysis.R
@@ -357,7 +357,7 @@ plot(model2$residuals, pch = 10, col = "red")
 
 model3 <- lm(mean_pollution ~ max_temp + min_temp + mean_temp , pollu_temp)
 summary(model3)
-anova(model3)
+anova(model3) 
 plot(model3$residuals, pch = 10, col = "red")
 
 plot(pollu_temp$max_temp, pollu_temp$mean_pollution)
